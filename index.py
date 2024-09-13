d4367eec0b64b4cb7ba564d9a7efa7c21687b95a
manfinder <programmersharp1379@gmail.com>
2024-09-13 17:36:44 +0330

    first commit from pomodoro

---
+++ index.py
@@ -0,0 +1,100 @@
+import time 
+import colorama
+from colorama import Fore 
+import os
+
+# seconds =time.time()
+
+# print("Time in seconds dince the epoch :",seconds)
+
+
+# local_time = time.ctime(seconds)
+
+# print("local time:", local_time)
+
+
+green=Fore.GREEN
+black=Fore.BLACK
+white=Fore.WHITE
+red=Fore.RED
+yellow=Fore.YELLOW
+blue=Fore.BLUE
+
+start=input()
+while start=='s':
+
+    print(start)
+    if start=='s':
+            
+        #atep 1:
+        os.system("mp3blaster 634090e627eea.mp3")
+
+        print("step 1 : "+red+"10 min "+green+"Practice ")
+        time.sleep(600)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 2:
+
+        print("Step 2: "+red+"10 min "+yellow+"Rest ")
+        time.sleep(600)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+        #step 3:
+
+        print("Step 3: "+red+"15 min "+green+"Practice ")
+        time.sleep(900)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 4:
+        print("Step 4: "+red+"5 min "+yellow+"Rest ")
+        time.sleep(300)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 5 :
+        print("Step 5: "+red+"20 min "+green+"Practice ")
+        time.sleep(1200)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 6: 
+
+        print("Step 6: "+red+"5 min "+yellow+"Rest ")
+        time.sleep(300)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 7:
+
+        print("Step 7: "+red+"25 min "+green+"Practice ")
+        time.sleep(1500)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print("Done !")
+        time.sleep(5)
+
+        #step 8:
+        print("Step 8: "+red+"5 min "+yellow+"Rest ")
+        time.sleep(300)
+        os.system("mp3blaster 634090e627eea.mp3")
+        print(green+"mission accomplished")
+
+
+
+
+
+
+
+
+
+
+
+
+
